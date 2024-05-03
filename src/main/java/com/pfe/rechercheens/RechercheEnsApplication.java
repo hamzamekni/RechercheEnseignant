bc2ce60eed3e150d9b039b3ef526509efb4295a2
hamzamekni <hamzamekni4@gmail.com>
2024-05-03 13:21:39 +0100

    new pfe project

---
+++ src/main/java/com/pfe/rechercheens/RechercheEnsApplication.java
@@ -0,0 +1,13 @@
+package com.pfe.rechercheens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechercheEnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RechercheEnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pfe/rechercheens/RechercheEnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe.rechercheens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RechercheEnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
